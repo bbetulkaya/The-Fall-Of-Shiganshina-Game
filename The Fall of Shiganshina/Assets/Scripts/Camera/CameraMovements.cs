959c8b4b372b34a444acd92f42408970873d64b2
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-11-14 15:46:11 +0300

    + Feat : add basic camera follow player script

---
+++ The Fall of Shiganshina/Assets/Scripts/Camera/CameraMovements.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovements : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    [SerializeField] public float smoothSpeed = 0.125f;
+    [SerializeField] public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        // Cam only follow z axis 
+        transform.position = new Vector3(transform.position.x, transform.position.y, smoothedPosition.z);
+    }
+}
